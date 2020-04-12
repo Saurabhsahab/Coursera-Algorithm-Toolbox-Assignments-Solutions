421f7911c3ea3fdf74138b3f23f32c67f6c4c92d
SAURABH UPADHAYAY <63358661+Saurabhsahab@users.noreply.github.com>
2020-04-12 23:16:12 +0530

    Week 2

--- coursera2.2.8.cpp
+++ coursera2.2.8.cpp
@@ -1,15 +1,26 @@
 //Saurabhsahab
-#include<bits/stdc++.h>
-using namespace std ;
-int main (){
-    int i=1;
-    do{
-        cout<<i<<"\a";
-        i++;
-        if(i<15)
-            continue;
-    }while(false);
-    while(1)
-        cout<<"\a";
+#include <bits/stdc++.h>
+#define max 100000000
+#define ll long long 
+using namespace std;
+vector<ll> fib;
+void fibonacci(long long n){
+	fib.push_back(0);
+	fib.push_back(1);
 
+	ll a;
+	for (int i = 2; i < n+1; ++i)
+	{
+		a = (fib[i-1]%10) + (fib[i-2]);
+		fib.push_back(a);
+	}
+}
+
+int main(){
+	long long n;
+	cin>>n;
+	fibonacci(n+1);
+	long long f = fib[n];
+	long long s = fib[n+1];
+	cout<<(f*s)%10;
 }
